ca7427386b27c6bd3276494c18f069dc01f961d6
Ashley Fajardo <a.e.fajardo06@gmail.com>
2026-04-25 19:16:29 -0600

    initial commit

---
+++ src/ashley/progra2/proyecto1/pkg2026/AshleyProgra2Proyecto12026.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ashley.progra2.proyecto1.pkg2026;
+
+/**
+ *
+ * @author ashley
+ */
+public class AshleyProgra2Proyecto12026 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
